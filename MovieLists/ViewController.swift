4562057814487d6e0852b025835c6cb0f43ce596
fsayik <f.sayik00@gmail.com>
2025-07-31 16:21:18 +0300

    Initial Commit

---
+++ MovieLists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieLists
+//
+//  Created by Ferhat Şayık on 31.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
